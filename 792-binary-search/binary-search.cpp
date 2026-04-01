b4230abea4fd3100ec4c14158f0ec4d95eccf536
pr9av <baliyanpranav5@gmail.com>
2026-04-02 03:23:34 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (45.03%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(left <= right) {
+            // Safe mid (important for large constraints)
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target) {
+                return mid;
+            }
+            else if(nums[mid] < target) {
+                // eliminate left half
+                left = mid + 1;
+            }
+            else {
+                // eliminate right half
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
